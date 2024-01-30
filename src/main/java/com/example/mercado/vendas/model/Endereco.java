f54b6f4aa18b2994fdff2b76a2a939f38248c096
arturbazza <agencia@fonzie.com.br>
2024-01-30 17:51:28 -0300

    Enviando projeto vendas com Cliente e Endereço

---
+++ src/main/java/com/example/mercado/vendas/model/Endereco.java
@@ -0,0 +1,16 @@
+package com.example.mercado.vendas.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Endereco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String logradouro;
+    private String bairro;
+}
